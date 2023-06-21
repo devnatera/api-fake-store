9f1b83b6ddf924885433a9992f241a43a1a1a9c5
devnatera <ednatera12@gmail.com>
2023-06-21 17:04:26 -0500

    se implementa manejo de errores con libreria boom

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -10,4 +10,17 @@ function errorHandler(err, req, res, next) {
   })
 }
 
-module.exports = { logErrors, errorHandler }
+function boomErrorHandler(err, req, res, next) {
+  if (err.isBoom) {
+    const { output } = err;
+    res.status(output.statusCode).json(output.payload);
+  } else {
+    next(err);
+  }
+}
+
+module.exports = {
+  logErrors,
+  boomErrorHandler,
+  errorHandler
+}
--- services/product.services.js
+++ services/product.services.js
@@ -1,4 +1,5 @@
 const { faker } = require('@faker-js/faker');
+const boom = require('@hapi/boom');
 
 class ProductService {
 
@@ -15,6 +16,7 @@ class ProductService {
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
         image: faker.image.url(),
+        isBlock: faker.datatype.boolean(),
       });
     }
   }
@@ -37,14 +39,20 @@ class ProductService {
   }
 
   async findOne(id) {
-    const name = this.getTotal();
-    return this.products.find(item => item.id === id);
+    const product = this.products.find(item => item.id === id);
+    if (!product) {
+      throw boom.notFound('product not found');
+    }
+    if (product.isBlock) {
+      throw boom.conflict('product is block');
+    }
+    return product;
   }
 
   async update(id, changes) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
-      throw new Error('product not found');
+      throw boom.notFound('product not found');
     }
     const product = this.products[index];
     this.products[index] = {
@@ -57,7 +65,7 @@ class ProductService {
   async delete(id) {
     const index = this.products.findIndex(item => item.id === id);
     if (index === -1) {
-      throw new Error('product not found');
+      throw boom.notFound('product not found');
     }
     this.products.splice(index, 1);
     return { id };
