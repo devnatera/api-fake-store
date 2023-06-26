5236fa674e9690f43dae95497045abf410a8784e
devnatera <devnatera@gmail.com>
2023-06-26 14:51:32 -0500

    se instala mysql2 y se implementa cambio de motor de base de datos desde las variables de entorno

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -6,10 +6,10 @@ const setupModels = require('../database/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `${config.dbDriver}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
+  dialect: config.dbDriver,
   logging: true,
 });
 
