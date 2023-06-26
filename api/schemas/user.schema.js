446975acda6fb32a4ef4b11a5b800ce6cd617c2c
devnatera <devnatera@gmail.com>
2023-06-26 15:42:03 -0500

    se agrega la propiedad role al modelo user, se agrega la migracion respectiva y se validacion de schema

--- api/schemas/user.schema.js
+++ api/schemas/user.schema.js
@@ -3,18 +3,17 @@ const Joi = require('joi');
 const id = Joi.number();
 const email = Joi.string().email();
 const password = Joi.string().min(8);
-// const role = Joi.string().min(5);
+const role = Joi.string().min(5);
 
 const createUserSchema = Joi.object({
   email: email.required(),
   password: password.required(),
-  // role: role.required(),
+  role: role.required(),
 });
 
 const updateUserSchema = Joi.object({
   email: email,
-  password: password,
-  // role: role,
+  role: role,
 });
 
 const getUserSchema = Joi.object({
